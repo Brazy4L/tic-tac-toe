a488c9ff0b8a081c8811a65e04e8fe0488d13ecf
Brazy <shutn1q@gmail.com>
2022-07-17 22:36:01 +0300

    Add winner check

--- js/script.js
+++ js/script.js
@@ -6,13 +6,14 @@ const Game = (() => {
                 if (play.innerText === '') {
                     play.innerText = flow();
                 }
+                check();
             })
         }
     }
-
+    
     let lastMove = '';
     let move = '';
-    
+
     const flow = () => {
         if (lastMove === '✕') {
             move = '⭘';
@@ -29,17 +30,25 @@ const Game = (() => {
         }
     }
 
-    const playerOne = () => {
-
+    const check = () => {
+        let one = document.getElementById('one').innerText;
+        let two = document.getElementById('two').innerText;
+        let three = document.getElementById('three').innerText;
+        let four = document.getElementById('four').innerText;
+        let five = document.getElementById('five').innerText;
+        let six = document.getElementById('six').innerText;
+        let seven = document.getElementById('seven').innerText;
+        let eight = document.getElementById('eight').innerText;
+        let nine = document.getElementById('nine').innerText;
+        if (one === '✕' && two === '✕' && three === '✕' || four === '✕' && five === '✕' && six === '✕' || seven === '✕' && eight === '✕' && nine === '✕' || one === '✕' && four === '✕' && seven === '✕' || two === '✕' && five === '✕' && eight === '✕' || three === '✕' && six === '✕' && nine === '✕' || one === '✕' && five === '✕' && nine === '✕' || three === '✕' && five === '✕' && seven === '✕') {
+            console.log('Player 1 is the winner!');
+        } else if (one === '⭘' && two === '⭘' && three === '⭘' || four === '⭘' && five === '⭘' && six === '⭘' || seven === '⭘' && eight === '⭘' && nine === '⭘' || one === '⭘' && four === '⭘' && seven === '⭘' || two === '⭘' && five === '⭘' && eight === '⭘' || three === '⭘' && six === '⭘' && nine === '⭘' || one === '⭘' && five === '⭘' && nine === '⭘' || three === '⭘' && five === '⭘' && seven === '⭘') {
+            console.log('Player 2 is the winner!');
+        }
     }
-
-    const playerTwo = () => {
-
-    }
-    
 
     return {
-        play,
+        play
     }
 })();
 
