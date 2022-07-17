ac58f3b426af4df6dfd7b3956c9b45d87e455345
Brazy <shutn1q@gmail.com>
2022-07-17 21:02:46 +0300

    Add logic

--- js/script.js
+++ js/script.js
@@ -1,3 +1,46 @@
 const Game = (() => {
+    const play = () => {
+        for (let i = 0; i < 9; i++) {
+            const play = document.querySelectorAll('.square')[i];
+            play.addEventListener('click', () => {
+                if (play.innerText === '') {
+                    play.innerText = flow();
+                }
+            })
+        }
+    }
 
+    let lastMove = '';
+    let move = '';
+    
+    const flow = () => {
+        if (lastMove === '✕') {
+            move = '⭘';
+            lastMove = '⭘';
+            return move;
+        } else if (lastMove === '⭘') {
+            move = '✕';
+            lastMove = '✕';
+            return move;
+        } else {
+            move = '✕';
+            lastMove = '✕';
+            return move;
+        }
+    }
+
+    const playerOne = () => {
+
+    }
+
+    const playerTwo = () => {
+
+    }
+    
+
+    return {
+        play,
+    }
 })();
+
+Game.play();
