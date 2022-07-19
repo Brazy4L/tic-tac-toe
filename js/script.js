0f966da716e7f8ac9f31ba5902484bfcb5a9bce0
Brazy <shutn1q@gmail.com>
2022-07-19 10:44:11 +0300

    Add animation

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 const Game = (() => {
+    const on = () => {
+        play();
+    }
+    
     const play = () => {
         for (let i = 0; i < 9; i++) {
             const play = document.querySelectorAll('.square')[i];
             play.addEventListener('click', () => {
                 if (play.innerText === '') {
                     play.innerText = flow();
+                    play.style = 'font-size: 65px;';
                 }
                 check();
             })
@@ -55,18 +60,31 @@ const Game = (() => {
         if (one === '✕' && two === '✕' && three === '✕' || four === '✕' && five === '✕' && six === '✕' || seven === '✕' && eight === '✕' && nine === '✕' || one === '✕' && four === '✕' && seven === '✕' || two === '✕' && five === '✕' && eight === '✕' || three === '✕' && six === '✕' && nine === '✕' || one === '✕' && five === '✕' && nine === '✕' || three === '✕' && five === '✕' && seven === '✕') {
             gameboard.classList.add('hide');
             winner.innerHTML = '<div><strong>✕</strong></div><div>WINNER!</div>';
-            numberLeft = +document.getElementById('score-x').innerText + 1;
-            document.getElementById('score-x').innerText = numberLeft;
+            winner.style = 'font-size: 48px';
+            numberLeft = +document.getElementById('score-x-top').innerText + 1;
+            document.getElementById('score-x-top').innerText = numberLeft;
+            document.getElementById('score-x-top').style = 'animation: mb 0.35s linear;'
+            document.getElementById('score-x').style = 'animation: mt 0.25s ease-out 0.10s;'
+            setTimeout(() => {
+                document.getElementById('score-x').innerText = numberLeft;
+            }, 335); 
             borderOff();
         } else if (one === '◯' && two === '◯' && three === '◯' || four === '◯' && five === '◯' && six === '◯' || seven === '◯' && eight === '◯' && nine === '◯' || one === '◯' && four === '◯' && seven === '◯' || two === '◯' && five === '◯' && eight === '◯' || three === '◯' && six === '◯' && nine === '◯' || one === '◯' && five === '◯' && nine === '◯' || three === '◯' && five === '◯' && seven === '◯') {
             gameboard.classList.add('hide');
             winner.innerHTML = '<div><strong>◯</strong></div><div>WINNER!</div>';
-            numberRight = +document.getElementById('score-o').innerText + 1;
-            document.getElementById('score-o').innerText = numberRight;
+            winner.style = 'font-size: 48px';
+            numberRight = +document.getElementById('score-o-top').innerText + 1;
+            document.getElementById('score-o-top').innerText = numberRight;
+            document.getElementById('score-o-top').style = 'animation: mb 0.35s linear;'
+            document.getElementById('score-o').style = 'animation: mt 0.25s ease-out 0.10s;'
+            setTimeout(() => {
+                document.getElementById('score-o').innerText = numberRight;
+            }, 335); 
             borderOff();
         } else if (one !== '' && two !== '' && three !== '' && four !== '' && five !== '' && six !== '' && seven !== '' && eight !== '' && nine !== '') {
             gameboard.classList.add('hide');
             winner.innerHTML = '<div><strong>✕◯</strong></div><div>DRAW!</div>';
+            winner.style = 'font-size: 48px';
             borderOff();
         }
     }
@@ -77,7 +95,14 @@ const Game = (() => {
         }
         lastMove = '';
         winner.innerHTML = '';
+        winner.style = '';
         gameboard.classList.remove('hide');
+        setTimeout(() => {
+            document.getElementById('score-x-top').style = ''
+            document.getElementById('score-x').style = ''
+            document.getElementById('score-o-top').style = ''
+            document.getElementById('score-o').style = ''
+        }, 400); 
         borderOn();
     }
 
@@ -87,19 +112,23 @@ const Game = (() => {
         document.querySelector('.square#five').style = 'border: none';
         document.querySelector('.square#six').style = 'border: none';
         document.querySelector('.square#eight').style = 'border: none';
+        document.querySelector('.square#one').style = '';
+        document.querySelector('.square#three').style = '';
+        document.querySelector('.square#seven').style = '';
+        document.querySelector('.square#nine').style = '';
     }
 
     const borderOn = () => {
-        document.querySelector('.square#two').style = 'border-right: 5px solid black; border-left: 5px solid black;';
-        document.querySelector('.square#four').style = 'border-top: 5px solid black; border-bottom: 5px solid black;';
-        document.querySelector('.square#five').style = 'border: 5px solid black;';
-        document.querySelector('.square#six').style = 'border-top: 5px solid black; border-bottom: 5px solid black;';
-        document.querySelector('.square#eight').style = 'border-right: 5px solid black; border-left: 5px solid black;';
+        document.querySelector('.square#two').style = '';
+        document.querySelector('.square#four').style = '';
+        document.querySelector('.square#five').style = '';
+        document.querySelector('.square#six').style = '';
+        document.querySelector('.square#eight').style = '';
     }
 
     return {
-        play,
+        on
     }
 })();
 
-Game.play();
+Game.on();
